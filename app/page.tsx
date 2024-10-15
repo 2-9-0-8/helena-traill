4d384aa46d12ec5c57dc0349b2f5baa5c25540df
Jerome Kenway <jerome@digitalmarmalade.co.uk>
2024-10-15 09:34:01 +0100

    no fallback homepage for now

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,6 @@ import { Icon } from '#/ui/icon'
 
 import { DESCRIPTION } from '#/lib/config'
 
-import { getContent } from '#/lib/queries.server'
-
 const [css, styles, href] = cxx`
 	.welcome {
 		animation: fade-in 1s ease-out forwards 1 1s;
@@ -21,23 +19,23 @@ const [css, styles, href] = cxx`
 		@media (width >= 720px) {
 			gap: var(--space-16x);
 		}
+	}
 
-		.intro, :global(.wp-block-heading) {
-			font-size: var(--text-2xl);
-			font-weight: 700;
-			margin-inline: auto;
-			max-inline-size: 50ch;
-			text-align: center;
-			text-wrap: balance;
+	.intro {
+		font-size: var(--text-2xl);
+		font-weight: 700;
+		margin-inline: auto;
+		max-inline-size: 50ch;
+		text-align: center;
+		text-wrap: balance;
 
-			@media (width >= 720px) {
-				font-size: var(--text-3xl);
-			}
+		@media (width >= 720px) {
+			font-size: var(--text-3xl);
 		}
+	}
 
-		.cta, :global(.btn) {
-			margin-inline: auto;
-		}
+	.cta {
+		margin-inline: auto;
 	}
 
 	@keyframes fade-in {
@@ -51,34 +49,19 @@ const [css, styles, href] = cxx`
 	}
 `
 
-function FallbackPage() {
-	return (
-		<div className={styles.welcome}>
-			<p className={styles.intro}>{`Hi 👋 ${DESCRIPTION}.`}</p>
-
-			<BlockButton as={Link} href="/archive/all" className={styles.cta}>
-				View my work <Icon name="arrow-right" title="Go" size={20} />
-			</BlockButton>
-		</div>
-	)
-}
-
 export default async function Page() {
-	const { contentNode } = await getContent('home', false)
-
 	return (
 		<>
-			<Title prefix={contentNode?.title ?? 'Home'} />
+			<Title prefix="Home" />
+			<h1 className="sr-only">Home</h1>
 
-			{contentNode && contentNode?.__typename === 'Page' ? (
-				<div
-					// biome-ignore lint/security/noDangerouslySetInnerHtml: don't be shook
-					dangerouslySetInnerHTML={{ __html: contentNode?.content ?? '' }}
-					className={styles.welcome}
-				/>
-			) : (
-				<FallbackPage />
-			)}
+			<div className={styles.welcome}>
+				<p className={styles.intro}>{`Hi 👋 ${DESCRIPTION}.`}</p>
+
+				<BlockButton as={Link} href="/archive/all" className={styles.cta}>
+					View my work <Icon name="arrow-right" title="Go" size={20} />
+				</BlockButton>
+			</div>
 
 			<style href={href} precedence="medium">
 				{css}
